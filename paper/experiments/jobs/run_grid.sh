0da7280d26ab8804904a25e408f1daed734020f1
pat-alt <altmeyerpat@gmail.com>
2024-11-05 12:19:57 +0100

    account

--- paper/experiments/jobs/run_grid.sh
+++ paper/experiments/jobs/run_grid.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=10
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=2G
-#SBATCH --account=innovation
+#SBATCH --account=research-eemcs-insy
 #SBATCH --mail-type=END
 
 module load 2024r1 
