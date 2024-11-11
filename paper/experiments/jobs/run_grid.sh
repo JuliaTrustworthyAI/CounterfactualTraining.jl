7dbf8789a0275fcedc8f4f2f72ea3de88bf0c947
pat-alt <altmeyerpat@gmail.com>
2024-11-11 11:43:28 +0100

    requeuing just does not seem to work

--- paper/experiments/jobs/run_grid.sh
+++ paper/experiments/jobs/run_grid.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --job-name="Run Grid"
 #SBATCH --partition=compute
-#SBATCH --time=00:30:00
+#SBATCH --time=01:00:00
 #SBATCH --ntasks=12
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=2G
@@ -14,9 +14,3 @@ module load 2024r1
 source paper/experiments/jobs/slurm_header.sh
 
 srun julia --project=$EXPERIMENT_DIR --threads $SLURM_CPUS_PER_TASK $EXPERIMENT_DIR/run_grid.jl > $LOG_DIR/run_grid.log
-
-# Only resubmit if the job timed out
-if grep -q "DUE TO TIME LIMIT" slurm-${SLURM_JOB_ID}.out 2>/dev/null; then
-    echo "Job timed out. Resubmitting..."
-    sbatch $0
-fi
