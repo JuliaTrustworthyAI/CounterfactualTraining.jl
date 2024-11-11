ca307087d4b4f0450dc77cbb3943ccf24e2a2208
pat-alt <altmeyerpat@gmail.com>
2024-11-11 14:09:55 +0100

    receive email on start

--- paper/experiments/jobs/run_grid.sh
+++ paper/experiments/jobs/run_grid.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=2G
 #SBATCH --account=research-eemcs-insy
-#SBATCH --mail-type=END,FAIL,TIME_LIMIT
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 
 module load 2024r1 
 
