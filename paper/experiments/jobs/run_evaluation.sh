5872cb2164d18e0a995d017edaad736f51df5de5
pat-alt <altmeyerpat@gmail.com>
2024-11-15 15:26:37 +0100

    just using multi-threading

--- paper/experiments/jobs/run_evaluation.sh
+++ paper/experiments/jobs/run_evaluation.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name="Run Evaluation"
 #SBATCH --partition=compute
 #SBATCH --time=01:00:00
-#SBATCH --ntasks=12
-#SBATCH --cpus-per-task=8
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=24
 #SBATCH --mem-per-cpu=4G
 #SBATCH --account=research-eemcs-insy
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
