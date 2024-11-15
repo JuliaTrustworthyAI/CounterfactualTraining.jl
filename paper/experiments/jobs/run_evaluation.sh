3798dfeb37b383ef9058a15bc080124519df114e
pat-alt <altmeyerpat@gmail.com>
2024-11-15 17:07:01 +0100

    vamos

--- paper/experiments/jobs/run_evaluation.sh
+++ paper/experiments/jobs/run_evaluation.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --job-name="Run Evaluation"
 #SBATCH --partition=compute
-#SBATCH --time=00:30:00
+#SBATCH --time=01:30:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
 #SBATCH --mem-per-cpu=2G
