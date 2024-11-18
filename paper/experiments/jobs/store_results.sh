da3b65c805363cd1f1a372b7151525266260d921
pat-alt <altmeyerpat@gmail.com>
2024-11-18 16:19:23 +0100

    buah

--- paper/experiments/jobs/store_results.sh
+++ paper/experiments/jobs/store_results.sh
@@ -2,8 +2,8 @@ source .env
 
 # Copy results to long-term storage:
 if [ $PWD = $CLUSTER_WORK_DIR ] ; then
-    echo 'Copying results to long term storage directory: $LONG_TERM_STORAGE_DIR'
-    cp -rf -n $OUTPUT_DIR $LONG_TERM_STORAGE_DIR
+    echo "Copying results to long term storage directory: $LONG_TERM_STORAGE_DIR"
+    cp -rf -n $OUTPUT_DIR $LONG_TERM_STORAGE_DIR/
 else
     echo 'It seems you are not in the cluster work directory. Skipping copy to long term storage.'
 fi
