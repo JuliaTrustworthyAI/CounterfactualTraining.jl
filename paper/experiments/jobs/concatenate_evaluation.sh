52036fedcdecd26a8766981f7668b3db268af8c8
pat-alt <altmeyerpat@gmail.com>
2024-11-20 09:48:16 +0100

    whoopsie

--- paper/experiments/jobs/concatenate_evaluation.sh
+++ paper/experiments/jobs/concatenate_evaluation.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=00:15:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=48
-#SBATCH --mem-per-cpu=16G
+#SBATCH --mem-per-cpu=2G
 #SBATCH --account=research-eemcs-insy
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 
