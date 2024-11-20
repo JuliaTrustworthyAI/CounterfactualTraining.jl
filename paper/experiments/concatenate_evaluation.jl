dee45d8f9d888c2adbed02d652a6f9fe7ae24212
pat-alt <altmeyerpat@gmail.com>
2024-11-20 09:06:23 +0100

    trying concat

--- paper/experiments/concatenate_evaluation.jl
+++ paper/experiments/concatenate_evaluation.jl
@@ -8,7 +8,7 @@ DotEnv.load!()
 
 # Get config and set up grid:
 eval_config = EvaluationConfig(
-    joinpath(ENV["EXPERIMENT_DIR"], "run_evaluation_config.toml")
+    joinpath(ENV["EXPERIMENT_DIR"], "evaluation_configs/2024-11-11.toml")
 )
 
 # Collect benchmarks:
