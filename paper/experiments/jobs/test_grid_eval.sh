cd05eed9f5d321196d54dbf5af1edc80edab2349
pat-alt <altmeyerpat@gmail.com>
2024-12-09 16:07:24 +0100

    ok?

---
+++ paper/experiments/jobs/test_grid_eval.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name="Test Grid Eval"
+#SBATCH --partition=compute
+#SBATCH --time=00:10:00
+#SBATCH --ntasks=10
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=2G
+#SBATCH --account=innovation
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
+
+module load 2024r1 
+
+source .env
+source $JOB_DIR/slurm_header.sh
+
+srun julia --project=$EXPERIMENT_DIR --threads $SLURM_CPUS_PER_TASK $EXPERIMENT_DIR/run_evaluation_grid.jl --config=$CONFIG > $LOG_DIR/run_evaluation_grid.log
