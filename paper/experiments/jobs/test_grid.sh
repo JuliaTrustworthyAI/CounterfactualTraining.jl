04503386aae808cfdde41df455c61ad7e6eb62f4
pat-alt <altmeyerpat@gmail.com>
2024-12-10 15:05:42 +0100

    I just don't get it

--- paper/experiments/jobs/test_grid.sh
+++ paper/experiments/jobs/test_grid.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="Test Grid"
 #SBATCH --partition=compute
 #SBATCH --time=00:10:00
-#SBATCH --ntasks=10
+#SBATCH --ntasks=12
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2G
 #SBATCH --account=innovation
