efa879c1ddd8c330be99de5eac4cb2f8b6ea3428
pat-alt <altmeyerpat@gmail.com>
2024-12-13 14:27:36 +0100

    gonna fail obv

--- paper/experiments/jobs/test_grid_eval.sh
+++ paper/experiments/jobs/test_grid_eval.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=20
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4G
-#SBATCH --account=research-eemcs-insy
+#SBATCH --account=innovation
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 
 module load 2024r1 
