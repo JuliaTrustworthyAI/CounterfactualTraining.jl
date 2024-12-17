9393af270cafb9a8dfd24c5f6515f6eee772346e
pat-alt <altmeyerpat@gmail.com>
2024-12-17 16:10:01 +0100

    new approach

--- paper/experiments/run_evaluation_grid.jl
+++ paper/experiments/run_evaluation_grid.jl
@@ -46,6 +46,25 @@ if length(eval_list) < nprocs
     @warn "There are less evaluations than processes. Check CPU efficiency of job."
 end
 chunks = TaijaParallel.split_obs(eval_list, nprocs)     # split  evaluations into chunks for each process
+
+# Set up dummies for processes without tasks to avoid deadlock:
+max_chunk_size = maximum(length.(chunks))
+chunks = Logging.with_logger(Logging.NullLogger()) do
+    for (i, chunk) in enumerate(chunks)
+        if length(chunk) < max_chunk_size
+            n_missing = max_chunk_size - length(chunk)
+            for j in 1:n_missing
+                cfg = deepcopy(eval_list[1])
+                cfg = make_dummy(cfg, i, j)
+                push!(chunk, cfg)
+            end
+        end
+    end
+    return chunks
+end
+
+@assert allequal(length.(chunks)) "Need all processes to have the same number of tasks."
+
 worker_chunk = MPI.scatter(chunks, comm)                # distribute across processes
 
 for (i, eval_config) in enumerate(worker_chunk)
