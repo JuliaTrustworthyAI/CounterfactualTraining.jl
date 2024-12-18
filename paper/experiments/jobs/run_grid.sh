7c955dd51df89ff8a70b271414864f92e131baea
pat-alt <altmeyerpat@gmail.com>
2024-12-18 11:13:30 +0100

    buf

--- paper/experiments/jobs/run_grid.sh
+++ paper/experiments/jobs/run_grid.sh
@@ -2,10 +2,10 @@
 #
 #SBATCH --job-name="Run Grid"
 #SBATCH --partition=compute
-#SBATCH --time=01:00:00
-#SBATCH --ntasks=100
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=2G
+#SBATCH --time=00:10:00
+#SBATCH --ntasks=6
+#SBATCH --cpus-per-task=12
+#SBATCH --mem-per-cpu=1G
 #SBATCH --account=research-eemcs-insy
 #SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 
