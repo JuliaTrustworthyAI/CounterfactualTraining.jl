ad5690dff83e679512dd956fe7b9a61d9b7457ff
pat-alt <altmeyerpat@gmail.com>
2024-12-19 19:53:46 +0100

    let's do as before

--- paper/experiments/jobs/run_grid_sequentially.sh
+++ paper/experiments/jobs/run_grid_sequentially.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name="Run Grid (Sequentially)"
 #SBATCH --partition=compute
 #SBATCH --time=01:00:00
-#SBATCH --ntasks=100
+#SBATCH --ntasks=40
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=2G
 #SBATCH --account=research-eemcs-insy
