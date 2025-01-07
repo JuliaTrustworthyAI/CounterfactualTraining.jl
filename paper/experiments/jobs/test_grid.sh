6c55219513624e77cbc4b6be64ab2c03262cf4e5
pat-alt <altmeyerpat@gmail.com>
2025-01-07 12:00:05 +0100

    try test grid

--- paper/experiments/jobs/test_grid.sh
+++ paper/experiments/jobs/test_grid.sh
@@ -2,8 +2,7 @@
 #
 #SBATCH --job-name="Test Grid"
 #SBATCH --partition=compute
-#SBATCH --time=00:15:00
-#SBATCH --ntasks=9
+#SBATCH --time=00:10:00
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=1G
 #SBATCH --account=innovation
@@ -14,4 +13,4 @@ module load 2024r1
 source .env
 source $JOB_DIR/slurm_header.sh
 
-srun julia --project=$EXPERIMENT_DIR --threads $SLURM_CPUS_PER_TASK $EXPERIMENT_DIR/run_grid.jl --config=$CONFIG > $LOG_DIR/run_grid.log
+srun julia --project=$EXPERIMENT_DIR --threads $SLURM_CPUS_PER_TASK $EXPERIMENT_DIR/run_grid.jl --config=$CONFIG
