ea6b45d082e9c48e3413c17519172752acb1a5ac
pat-alt <altmeyerpat@gmail.com>
2025-01-15 12:32:32 +0100

    flying right now

--- paper/experiments/jobs/slurm_header.sh
+++ paper/experiments/jobs/slurm_header.sh
@@ -20,6 +20,20 @@ for arg in "$@"; do
     fi
 done
 
-TIMELIMIT=$(scontrol show job $SLURM_JOB_ID | awk -F= '/TimeLimit/ {print $2}')
+# Extract time limit
+RAW_TIMELIMIT=$(scontrol show job $SLURM_JOB_ID | awk -F= '/TimeLimit/ {print $2}')
+
+# Convert DD-HH:MM:SS to HH:MM:SS
+if [[ "$RAW_TIMELIMIT" == *-* ]]; then
+  # Split into days and time
+  DAYS=${RAW_TIMELIMIT%-*}
+  TIME=${RAW_TIMELIMIT#*-}
+  # Convert days to hours
+  HOURS=$((DAYS * 24 + ${TIME%%:*}))
+  MINUTES=${TIME#*:}
+  TIMELIMIT="$HOURS:$MINUTES"
+else
+  TIMELIMIT=$RAW_TIMELIMIT
+fi
 
 echo "JOB DETAILS: Running on $SLURM_NTASKS CPUs with $SRUN_CPUS_PER_TASK threads per cpu for $TIMELIMIT (hh:mm:ss)"
