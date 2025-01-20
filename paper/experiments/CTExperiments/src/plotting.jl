4d1cd598cf6fcfd93f5d263dc85db7ad94a02e14
pat-alt <altmeyerpat@gmail.com>
2025-01-20 16:21:11 +0100

    va

--- paper/experiments/CTExperiments/src/plotting.jl
+++ paper/experiments/CTExperiments/src/plotting.jl
@@ -105,6 +105,7 @@ function aggregate_data(
     byvars_must_include::Union{Nothing,Vector{String}}=nothing,
 )
     df = filter(row -> all(x -> !(x isa Number && (isinf(x))), row), df)
+    df = df[.!isnan.(df[:,y]), :]
     if "value" in names(df)
         df = df[.!isnan.(df.value),:]
     end
