de6b2786dd358474dcf12ecb009b9bb4f51a3d6a
pat-alt <altmeyerpat@gmail.com>
2025-02-05 16:13:33 +0100

    progress

--- src/implausibility.jl
+++ src/implausibility.jl
@@ -7,7 +7,8 @@ using LinearAlgebra
 Compute the implausibility (contrastive divergence) of the counterfactuals (`perturbed_input`) with respect to `samples` in the target class. This is computed as the difference between negative logits indexed at the target class for the `samples` and the `perturbed_input`.
 """
 function implausibility(model, perturbed_input, samples, targets)
-    x = ((-model(samples)) - (-model(perturbed_input)))'targets
+    E(x) = -model(x)                                    # energy
+    x = ((E(samples)) - (E(perturbed_input)))'targets   # contrastive divergence
     return diag(x[:, :])
 end
 
