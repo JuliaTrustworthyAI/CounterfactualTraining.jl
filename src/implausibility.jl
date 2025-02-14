f8fbe423f1da6afe7b8187e026e97191e27c6e15
pat-alt <altmeyerpat@gmail.com>
2025-02-14 15:48:17 +0100

    mmm trying to do grads with respect to implausibility but not clear how to plot

--- src/implausibility.jl
+++ src/implausibility.jl
@@ -7,9 +7,8 @@ using LinearAlgebra
 Compute the implausibility (contrastive divergence) of the counterfactuals (`counterfactual`) with respect to `samples` in the target class. This is computed as the difference between negative logits indexed at the target class for the `samples` and the `counterfactual`.
 """
 function implausibility(model, counterfactual, samples, targets)
-    E(x) = -model(x)                                            # energy
-    x = ((E(samples)) - (E(counterfactual)))[:, :]'targets       # contrastive divergence
-    # x = ((E(counterfactual)) - (E(samples)))'targets   # contrastive divergence
+    E(x) = -model(x)                                        # energy
+    x = ((E(samples)) - (E(counterfactual)))[:, :]'targets  # contrastive divergence
     return diag(x[:, :])
 end
 
