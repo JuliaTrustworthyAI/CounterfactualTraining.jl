434ca66c7e00860b5e20a6191ebb2e6092675d39
pat-alt <patalt@patalt.org>
2026-08-06 16:26:43 +0200

    created module directory and removed redundant line

--- src/CounterfactualTraining.jl
+++ src/CounterfactualTraining.jl
@@ -9,4 +9,24 @@ export implausibility, reg_loss
 include("training.jl")
 export counterfactual_training
 
+module Native
+    import ..CounterfactualTraining: counterfactual_training
+    import ..CounterfactualTraining: AbstractObjective
+    import ..CounterfactualTraining: implausibility, reg_loss
+    import ..CounterfactualTraining: infer_domain_constraints, unwrap
+    import ..CounterfactualTraining: VanillaObjective, EnergyDifferentialObjective
+    import ..CounterfactualTraining: AdversarialObjective, FullObjective
+
+    include("native/data.jl")
+    include("native/convergence.jl")
+    include("native/generator.jl")
+    include("native/counterfactuals.jl")
+    include("native/training.jl")
+
+    export AbstractNativeGenerator, ECCoGenerator
+    export NativeCFData
+    export MaxIterConvergence, DecisionThresholdConvergence
+end
+export Native
+
 end
